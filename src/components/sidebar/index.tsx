211504e27aeaae20685fef555eb59f7ebb2ee80c
Hilquias Ferreira Melo <hilquiasfmelo@hotmail.com>
2023-08-02 12:51:46 -0300

    feat:Pattern de composicao

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,9 +1,10 @@
 import { Cog, LifeBuoy, Search } from 'lucide-react'
 
-import { MainNavigation } from './main-navigation'
-import { NavItem } from './main-navigation/nav-item'
 import { UsedSpaceWidget } from './used-space-widget'
+import { NavItem } from './main-navigation/nav-item'
+import { MainNavigation } from './main-navigation'
 import { Profile } from './profile'
+import * as Input from '../input'
 
 import { Logo } from './logo'
 
@@ -12,14 +13,12 @@ export function Sidebar() {
     <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
 
-      <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
-        <Search className="h-5 w-5 text-zinc-500" />
-        <input
-          type="text"
-          placeholder="Search"
-          className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
-        />
-      </div>
+      <Input.Root>
+        <Input.Prefix>
+          <Search className="w5 h-5 text-zinc-500" />
+        </Input.Prefix>
+        <Input.Control placeholder="Search" />
+      </Input.Root>
 
       <MainNavigation />
 
