86fd70022d514aea959725f57fac31ed9d02aba4
hilquiasfmelo <hilquiasfmelo@hotmail.com>
2023-08-01 22:33:36 -0300

    feat:perfil de usuario

---
+++ src/components/sidebar/main-navigation/index.tsx
@@ -0,0 +1,23 @@
+import {
+  BarChart,
+  CheckSquare,
+  Flag,
+  Home,
+  SquareStack,
+  User,
+} from 'lucide-react'
+
+import { NavItem } from './nav-item'
+
+export function MainNavigation() {
+  return (
+    <nav className=" space-y-0.5">
+      <NavItem title="Home" icon={Home} />
+      <NavItem title="Dashboard" icon={BarChart} />
+      <NavItem title="Projects" icon={SquareStack} />
+      <NavItem title="Tasks" icon={CheckSquare} />
+      <NavItem title="Reporting" icon={Flag} />
+      <NavItem title="Users" icon={User} />
+    </nav>
+  )
+}
