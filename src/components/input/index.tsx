07da1f21b133a7f8de5d4041ddb876bfb5fa5faa
Hilquias Ferreira Melo <hilquiasfmelo@hotmail.com>
2023-08-02 17:10:02 -0300

    feat:estrutura do formulario

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,5 +1,16 @@
 import { ComponentProps } from 'react'
 
+type InputRootProps = ComponentProps<'div'>
+
+export function Root(props: InputRootProps) {
+  return (
+    <div
+      className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+      {...props}
+    ></div>
+  )
+}
+
 type InputPrefixProps = ComponentProps<'div'>
 
 export function Prefix(props: InputPrefixProps) {
@@ -15,16 +26,5 @@ export function Control(props: InputControlProps) {
       className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
       {...props}
     />
-  )
-}
-
-type InputRootProps = ComponentProps<'div'>
-
-export function Root(props: InputRootProps) {
-  return (
-    <div
-      className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
-      {...props}
-    ></div>
   )
 }
